67357597309d898740a148f696e2d479ceec0027
radd-ko <raddkof2@gmail.com>
2015-01-10 12:05:28 +0200

    Update PrintASCII.cs

--- Homeworks/Data-Types-Variables/ASCIITable/PrintASCII.cs
+++ Homeworks/Data-Types-Variables/ASCIITable/PrintASCII.cs
@@ -5,6 +5,7 @@
     {
         static void Main()
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             for (int i = 0; i <= 255; i++)
             {
                 System.Console.WriteLine("{0} = {1}", i, (char)i);
