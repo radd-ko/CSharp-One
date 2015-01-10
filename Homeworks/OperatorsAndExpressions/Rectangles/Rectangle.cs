10282bec6fa06a293b4b8e92ecb275a0bad3c02a
radd-ko <raddkof2@gmail.com>
2015-01-10 11:15:11 +0200

    Update Rectangle.cs

--- Homeworks/OperatorsAndExpressions/Rectangles/Rectangle.cs
+++ Homeworks/OperatorsAndExpressions/Rectangles/Rectangle.cs
@@ -11,8 +11,8 @@
            double height = double.Parse(Console.ReadLine());
            double perimeter = 2 * (width + height);
            double area = width * height;
-           Console.WriteLine("the perimeter is: {0:N2}cm", perimeter);
-           Console.WriteLine("the area is {0:N2}cm{1}", area, (char)0xB2);
+           Console.WriteLine("the perimeter is: {0:F2}cm", perimeter);
+           Console.WriteLine("the area is {0:F2}cm{1}", area, (char)0xB2);
         }
     }
 }
